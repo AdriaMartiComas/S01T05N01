fec1046dd0cba5e4a0efaf5bcf6361a259bafa14
adriamarticomas <adriamarticomas@localhost>
2022-07-08 10:26:40 +0200

    com

---
+++ S01T05N01/src/exercici2/OrdenarAlfabeticament.java
@@ -0,0 +1,44 @@
+package exercici2;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class OrdenarAlfabeticament {
+
+	public static void main(String[] args) {
+		ArrayList<String> llista = new ArrayList<String>();
+		
+		importText(llista);
+		System.out.println(llista);
+				
+		Collections.sort(llista);
+		System.out.println(llista);
+	
+
+	}
+	
+
+	
+	public static void importText(ArrayList<String> llista) {
+
+		try {
+			BufferedReader inputText = new BufferedReader(new FileReader("dir.txt"));
+			String linea;
+
+			while ((linea = inputText.readLine()) != null) {
+
+				llista.add(linea.toString());
+
+			}	
+			inputText.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
